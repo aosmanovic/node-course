4d7c74fddd99865e5ee84f1dbdd1506d3f7ca7fa
Anela <osmanovic.anela@gmail.com>
2017-07-31 16:56:59 +0200

    Setup start script and heroku port

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-const app = express();
+const port = process.env.PORT || 3000;
+
+var app = express();
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,5 +15,9 @@ require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.'
 }));
+
+app.listen(port, () => {
+  console.log('Server is up on port');
+});
 
 module.exports = app;
