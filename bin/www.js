2d5710b93aabd595c296b4887b208e9fe88bbb2a
Anela <osmanovic.anela@gmail.com>
2017-07-31 11:46:50 +0200

    Initial commit

---
+++ bin/www.js
@@ -0,0 +1,14 @@
+// This will be our application entry. We'll setup our server here.
+const http = require('http');
+const app = require('../app'); // The express app we just created
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.on('listening', function () {
+  console.log('ok, server is running');
+});
+
+server.listen(port);
